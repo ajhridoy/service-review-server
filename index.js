e1398b05117d1ef71466e2289471e32df1cde96a
ajhridoy <ajhridoy461@gmail.com>
2022-11-09 00:10:14 +0600

    added service details page API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,13 @@ async function run(){
             const cursor = photographyServices.find(query)
             const services = await cursor.toArray()
             res.send(services)
+        })
+
+        app.get('/serviceDetails/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await photographyServices.findOne(query)
+            res.send(service)
         })
         
 
