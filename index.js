ff6106038eedc5484b7da4acb3eec2243ce4e89a
ajhridoy <ajhridoy461@gmail.com>
2022-11-08 19:18:37 +0600

    connected with mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.jh5ecod.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
     res.send('service review is running')
