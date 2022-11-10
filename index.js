6a56edaa9ef6892b7906003a932bae3721f4332c
ajhridoy <ajhridoy461@gmail.com>
2022-11-10 19:26:58 +0600

    implement JWT token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -8,7 +9,20 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message: 'Unauthorize Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            res.status(401).send({message: 'Unauthorize Access'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.jh5ecod.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -17,13 +31,6 @@ async function run(){
     try{
         const photographyServices = client.db('serviceReview').collection('services')
         const reviewsCollection = client.db('serviceReview').collection('reviews')
-
-        app.get('/serviceshome', async(req, res) => {
-            const query = {}
-            const cursor = photographyServices.find(query)
-            const services = await cursor.limit(3).toArray()
-            res.send(services)
-        })
 
         app.get('/services', async(req, res) => {
             const query = {}
@@ -47,6 +54,12 @@ async function run(){
 
         //review collection
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+            res.send({token})
+        })
+
         app.get('/reviews', async(req, res) => {
             let query = {}
             if(req.query.itemId){
@@ -60,7 +73,11 @@ async function run(){
             res.send(reviews)
         })
 
-        app.get('/myreviews', async(req, res) => {
+        app.get('/myreviews', verifyJWT, async(req, res) => {
+            const decoded = req.decoded
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'Forbidden Access'})
+            }
             let query = {}
             if(req.query.email){
                 query = {
